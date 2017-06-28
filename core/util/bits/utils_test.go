9873754ea8b1af69ea68beb8f47a1d12cd5604ae
Lawrence <lawrencepeng233@gmail.com>
2017-06-28 22:39:46 +0800

    add test/bittest

--- core/util/bits/utils_test.go
+++ core/util/bits/utils_test.go
@@ -2,11 +2,56 @@ package bits
 
 import "testing"
 import (
-	_ "github.com/lexandro/go-assert"
 	"github.com/lexandro/go-assert"
 )
 
 func TestGetAndSetBit(t *testing.T) {
 	n := int64(0)
-	assert.Equals(t, )
+
+	for i := 0; i < 64; i++ {
+		assert.Equals(t, 0, int(GetBit(n , i)))
+	}
+
+	for i := 0; i < 64; i++ {
+		n = SetBit(n, i)
+		for j := 0; j < 64; j++ {
+			if i == j {
+				assert.Equals(t, 1, int(GetBit(n, j)))
+			} else {
+				assert.Equals(t, 0, int(GetBit(n, j)))
+			}
+		}
+		n = 0
+	}
+
+	for i := 0; i < 64; i++ {
+		n = SetBit(n, i)
+		for j := 0; j < 64; j++ {
+			if j <= i {
+				assert.Equals(t, 1, int(GetBit(n, j)))
+			} else {
+				assert.Equals(t, 0, int(GetBit(n, j)))
+			}
+		}
+	}
+}
+
+func TestBitsToBlock64(t *testing.T) {
+	assert.Equals(t,0, int(BitsToBlock64(0)))
+	for i := 1; i <= 64; i++ {
+		assert.Equals(t,1, int(BitsToBlock64(int64(i))))
+	}
+	assert.Equals(t,2, int(BitsToBlock64(int64(65))))
+	assert.Equals(t, 4, int(BitsToBlock64(int64(256))))
+	assert.Equals(t, 5, int(BitsToBlock64(int64(257))))
+}
+
+func TestBitWidth(t *testing.T) {
+	assert.Equals(t,1, BitWidth(0))
+	assert.Equals(t,1, BitWidth(1))
+	assert.Equals(t,2, BitWidth(2))
+	assert.Equals(t,2, BitWidth(3))
+	assert.Equals(t,3, BitWidth(4))
+	assert.Equals(t,8, BitWidth(255))
+	assert.Equals(t, 9, BitWidth(256))
 }
